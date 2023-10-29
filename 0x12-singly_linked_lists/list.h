cef2ce1882c3aaefb61ec375e0b3dc29548a07d5
MsKathure <kmmbaabu@gmail.com>
2023-10-29 03:47:43 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -5,20 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ 12   * struct list_s - stringly linked list
+ 13   * @str: string (malloced string)
+ 14   * @len: string length
+ 15   * @next: pointer to the next node
+ 16   */
+ 17
+ 18 typedef struct list_s
+ 19 {
+ 20         char *str;
+ 21         size_t len;
+ 22         struct list_s *next;
+ 23 } list_t
+
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 
-/**
-  * struct list_s - stringly linked list
-  * @str: string (malloced string)
-  * @len: string length
-  * @next: pointer to the next node
-  */
-
-typedef struct list_s
-{
-	char *str;
-	size_t len;
-	struct list_s *next;
-} list_t
 #endif
