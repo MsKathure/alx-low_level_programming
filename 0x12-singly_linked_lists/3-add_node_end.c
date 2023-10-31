29371d42d4edef31c6f44225dcab52578da2078e
MsKathure <kmmbaabu@gmail.com>
2023-10-31 15:03:43 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,12 +12,11 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t *temp = *head;
+	list_t *temp = *head; 
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
-
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
@@ -31,6 +30,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new;
 		return (new);
 	}
+
 	while (temp->next)
 		temp = temp->next;
 
